b32f73ab4e5da863fb34c7194049e88b83d00eff
SARAVANAKUMAR N <096saravanakumarn@gmail.com>
2024-10-17 17:46:15 +0530

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, nam in error omnis explicabo obcaecati repellat suscipit corrupti quod rem neque ab minus possimus rerum sunt tempora praesentium reprehenderit a.
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
